dadfc0f29fb65b6cb218097589b259b8b51ab626
Ambar Mutha <ambar.ytl@gmail.com>
2020-02-06 20:19:46 +0530

    fix missing extends component

--- App.tsx
+++ App.tsx
@@ -1,36 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaView, Text, StatusBar, StyleSheet } from 'react-native';
 import { Appbar, FAB } from 'react-native-paper';
 import Main from './components/Main';
 import Signin from './components/Signin';
 
-export default class App {
-
+export default class App extends Component {
 	render() {
-		if (isusersignedin())
-			return (
-
-				<Main />
-			);
-
-			else{
-				return <Signin/>
-			}
+		if (isusersignedin()) return <Main />;
+		else {
+			return <Signin />;
+		}
 	}
 }
 
-	const styles = StyleSheet.create({
-		fab: {
-			position: 'absolute',
-			margin: 16,
-			alignSelf: 'center',
-			backgroundColor: 'red',
-			bottom: 0,
-		},
-	});
+const styles = StyleSheet.create({
+	fab: {
+		position: 'absolute',
+		margin: 16,
+		alignSelf: 'center',
+		backgroundColor: 'red',
+		bottom: 0,
+	},
+});
 
 function isusersignedin() {
 	return false;
-	
 }
 //export default App;
--- components/Main.tsx
+++ components/Main.tsx
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 import { SafeAreaView, Text, StatusBar, StyleSheet } from 'react-native';
 import { Appbar, FAB } from 'react-native-paper';
 
-export default class Main extends Component{
-
-	render()
-	{
-	return (
-		<>
-			<StatusBar barStyle="light-content" />
-			<Appbar>
-				<Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
-				<Appbar.Action icon="email" onPress={() => console.log('Pressed mail')} />
-				<Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-				<Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
-			</Appbar>
-			<SafeAreaView>
-				<Text>App works!</Text>
-			</SafeAreaView>
-			<FAB style={styles.fab} label="New" icon="plus" onPress={() => console.log('Pressed plus')} />
-		</>
+export default class Main extends Component {
+	render() {
+		return (
+			<>
+				<StatusBar barStyle="light-content" />
+				<Appbar>
+					<Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
+					<Appbar.Action icon="email" onPress={() => console.log('Pressed mail')} />
+					<Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+					<Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
+				</Appbar>
+				<SafeAreaView>
+					<Text>App works!</Text>
+				</SafeAreaView>
+				<FAB style={styles.fab} label="New" icon="plus" onPress={() => console.log('Pressed plus')} />
+			</>
+		);
 	}
-	);
-};
+}
 
 const styles = StyleSheet.create({
 	fab: {
