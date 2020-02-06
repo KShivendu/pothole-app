18820bcc053c23f6973e3f28d16891e5abb53d92
GAGANDEEP SINGH <gagandeep231020@gmail.com>
2020-02-06 14:45:02 +0530

    added paper to sign in page

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, Button,TextInput,Alert} from 'react-native';
-
+import { View, Text,Alert} from 'react-native';
+import {Provider as PaperProvider,Button,TextInput} from 'react-native-paper';
 export default class Signin extends Component {
     render() {
-        return (<View style={{ flex: 1 }}>
+        return (
+            <PaperProvider>
+        <View style={{ flex: 1 }}>
             <View style={{ flex: 1 }}></View>
-            <View style={{ flex: 1}}>
-                <Text style={{ fontSize: 25,margin:25,textAlign:"center" }}>Enter your phone number to get Started</Text>
-                <TextInput></TextInput>
+            <View style={{ flex: 2}}>
+                <Text style={{ fontSize: 26,margin:25,textAlign:"center" }}>Enter your phone number to get Started</Text>
+                <TextInput label="Mobile No."  mode="outlined" style={{margin:20}}></TextInput>
                 <View style={{width:150,alignSelf:"center"}}>
-                <Button title="Get Started"  onPress={() => Alert.alert('Simple Button pressed')}/></View>
+               <Button mode="contained" onPress={() => Alert.alert('Simple Button pressed')}>Get Started</Button></View>
                   </View>
             <View style={{ flex: 1 }}></View>
-        </View>);
+        </View>
+        </PaperProvider>);
     }
 }
