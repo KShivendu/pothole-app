927c7cd0e1338bcef90a47e572606fa294e70d25
Ambar Mutha <ambar.ytl@gmail.com>
2020-02-06 16:30:05 +0530

    fix icon issue

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 			<StatusBar barStyle="light-content" />
 			<Appbar>
 				<Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
-				<Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+				<Appbar.Action icon="email" onPress={() => console.log('Pressed mail')} />
 				<Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
 				<Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
 			</Appbar>
