48c831adbbe68a8b5d9dedaafc05b4f43c195d69
GAGANDEEP SINGH <gagandeep231020@gmail.com>
2020-02-07 16:31:01 +0530

    camera working now

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { SafeAreaView, Text, StatusBar, StyleSheet } from 'react-native';
-import { Appbar, FAB } from 'react-native-paper';
-import { NavigationProp } from '@react-navigation/native';
-import { RNCamera } from 'react-native-camera';
-import { Provider as PaperProvider, Button } from 'react-native-paper';
+import React, {Component} from 'react';
+import {SafeAreaView, Text, StatusBar, StyleSheet, View} from 'react-native';
+import {Appbar, FAB} from 'react-native-paper';
+import {NavigationProp} from '@react-navigation/native';
+import {RNCamera} from 'react-native-camera';
+import {Provider as PaperProvider, Button} from 'react-native-paper';
 import RNLocation from 'react-native-location';
 import {Image} from 'react-native-paper/lib/typescript/src/components/Avatar/Avatar';
 
@@ -22,7 +22,7 @@ export default class Camera extends Component {
 	async clickphoto() {
 		if (this.camera) {
 			this.data = await this.camera.takePictureAsync({base64: true});
-			this.setState({lev:1})
+			this.setState({lev: 1});
 			// console.log('base64: ', data.base64);
 			this.findCoordinates();
 			//this.state={transition:false}
@@ -59,7 +59,8 @@ export default class Camera extends Component {
                     fromMockProvider: false
                   }
                   */
-				});
+					},
+				);
 			}
 		});
 	};
@@ -86,7 +87,13 @@ export default class Camera extends Component {
 				</PaperProvider>
 			);
 		} else {
-			return <Image source={this.data}></Image>;
+			return (
+				<Button
+					loading={true}
+					style={{flex: 1, justifyContent: 'center', height: 60}}>
+					{' '}
+				</Button>
+			);
 		}
 	}
 }
