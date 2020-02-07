4199d0fd557b6808842dc2eacc5864dfeae183c9
KShivendu <shivendu@iitbhilai.ac.in>
2020-02-07 14:43:46 +0530

    XHR Request

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { SafeAreaView, Text, StatusBar, StyleSheet } from 'react-native';
-import { Appbar, FAB } from 'react-native-paper';
-import { NavigationProp } from '@react-navigation/native';
+import React, {Component} from 'react';
+import {SafeAreaView, Text, StatusBar, StyleSheet} from 'react-native';
+import {Appbar, FAB, Button} from 'react-native-paper';
+import {NavigationProp} from '@react-navigation/native';
 
 export default class Main extends Component {
 	navigation: NavigationProp<any>;
@@ -17,13 +17,28 @@ export default class Main extends Component {
 			<>
 				<StatusBar barStyle="light-content" />
 				<Appbar>
-					<Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
-					<Appbar.Action icon="email" onPress={() => console.log('Pressed mail')} />
-					<Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-					<Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
+					<Appbar.Action
+						icon="archive"
+						onPress={() => console.log('Pressed archive')}
+					/>
+					<Appbar.Action
+						icon="email"
+						onPress={() => console.log('Pressed mail')}
+					/>
+					<Appbar.Action
+						icon="label"
+						onPress={() => console.log('Pressed label')}
+					/>
+					<Appbar.Action
+						icon="delete"
+						onPress={() => console.log('Pressed delete')}
+					/>
 				</Appbar>
 				<SafeAreaView>
 					<Text>App works!</Text>
+					<Button icon="camera" mode="contained" onPress={() => sendImage()}>
+						Press me
+					</Button>
 				</SafeAreaView>
 				<FAB
 					style={styles.fab}
@@ -45,3 +60,62 @@ const styles = StyleSheet.create({
 		bottom: 0,
 	},
 });
+
+const sendImage = () => {
+	var url = 'http://192.168.43.90:5000/cross'; // File upload web service path
+
+	var request = new XMLHttpRequest();
+	request.setRequestHeader("Content-type", "multipart/form-data");
+	request.onreadystatechange = e => {
+		if (request.readyState !== 4) {
+			return;
+		}
+
+		if (request.status === 200) {
+			console.log('success', request.responseText);
+		} else {
+			console.warn('error');
+		}
+	};
+
+	request.open('GET', url);
+	request.send();
+
+	var photo = {
+		uri: this.state.picturePath, // CameralRoll Url
+		type: 'image/jpeg',
+		name: 'photo.jpg',
+	};
+  
+	var formData = new FormData();
+	formData.append("file", photo);
+  
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', url);
+	console.log('OPENED', xhr.status);
+  
+	xhr.onprogress = function () {
+		console.log('Sending', xhr.status);
+	};
+  
+	xhr.onload = function () {
+		console.log('DONE', xhr.status);
+	};
+	
+	xhr.setRequestHeader('authorization', this.state.token);
+	xhr.send(formData);
+  
+
+	// request.open('GET', 'http://10.3.7.86:5000/cross/');
+	
+
+
+	// return fetch('http://10.3.7.86:5000/cross/')
+	// 	.then(response => response.json())
+	// 	.then(responseJson => {
+	// 		console.log(responseJson);
+	// 	})
+	// 	.catch(error => {
+	// 		console.error(error);
+	// 	});
+};
